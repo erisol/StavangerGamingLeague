83ba2bc231fc3e79452000113e78e8b63d0ff781
KevinMcTiernan <kevin.tiernan@knowit.no>
2018-03-01 11:25:25 +0100

    Intial commit, sprinboot base

---
+++ src/main/java/com/knowit/laddergoat/LaddergoatApplication.java
@@ -0,0 +1,12 @@
+package com.knowit.laddergoat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaddergoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaddergoatApplication.class, args);
+	}
+}
